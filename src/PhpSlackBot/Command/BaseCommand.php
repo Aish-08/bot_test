3d1e979ec3e6d7b903c7a5a3f6f8408065581c9d
Aish-08 <112920638+Aish-08@users.noreply.github.com>
2022-09-06 16:45:26 +0530

    Add files via upload

---
+++ src/PhpSlackBot/Command/BaseCommand.php
@@ -0,0 +1,11 @@
+<?php
+namespace PhpSlackBot\Command;
+
+abstract class BaseCommand extends \PhpSlackBot\Base
+{
+
+    public function executeCommand($message, $context)
+    {
+        return $this->execute($message, $context);
+    }
+}
---
+++ src/PhpSlackBot/Webhook/BaseWebhook.php
@@ -0,0 +1,11 @@
+<?php
+namespace PhpSlackBot\Webhook;
+
+abstract class BaseWebhook extends \PhpSlackBot\Base
+{
+
+    public function executeWebhook($payload, $context)
+    {
+        return $this->execute($payload, $context);
+    }
+}
